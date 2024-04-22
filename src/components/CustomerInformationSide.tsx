aaa8ff8b0da97910ad5a9edef22598983fa11653
Mikkel Amskov <mikkel@amskov.dk>
2024-04-22 20:24:04 +0200

    total med over

--- src/components/CustomerInformationSide.tsx
+++ src/components/CustomerInformationSide.tsx
@@ -3,15 +3,21 @@ import React, {useState} from 'react';
 import AddressForm, {AddressFields} from "./AddressForm";
 import DeliveryAddress from "./DeliveryAddress.tsx";
 import PaymentForm from "./PaymentForm.tsx";
+import Total1 from "./Total1.tsx";
 
 interface Item {
     name: string;
+    price: number;
     quantity: number;
+    ImageURL: string;
+    giftWrap?: boolean;
+    recurring?: string;
 }
 interface CustomerProps {
     items: Item[];
     totalAmount: number;
 }
+
 
 const CheckoutPage:React.FC<CustomerProps> = ({items, totalAmount}) => {
     const [companyVAT, setCompanyVAT] = useState('');
@@ -20,10 +26,14 @@ const CheckoutPage:React.FC<CustomerProps> = ({items, totalAmount}) => {
     const handleSubmitAddress = (address: AddressFields) => {
         setAddressInfo(address); // Update the address info state
     };
+    const handleUpdateTotal = () => {};
 
     return (
         <div className={"page-column"}>
             <div className="right-side1">
+                <h2> Basket </h2>
+                <Total1 items={items} onUpdateTotal={handleUpdateTotal}   />
+
                 <h2> Customer information </h2>
                 <AddressForm
                     onCompanyVATChange={setCompanyVAT}
