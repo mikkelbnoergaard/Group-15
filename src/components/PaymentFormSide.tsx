553eb5a87e9b56b8dbad6604b0306495fbd3ecc7
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2024-04-29 11:45:30 +0200

    changed total1 to total, and changed fanetitel

--- src/components/PaymentFormSide.tsx
+++ src/components/PaymentFormSide.tsx
@@ -1,6 +1,6 @@
 import './BasketSide.css';
 import PaymentForm from "./PaymentForm.tsx";
-import Total1 from "./Total1.tsx";
+import Total from "./Total.tsx";
 import {useNavigate} from "react-router-dom";
 import {PaymentInformation} from "./PaymentForm.tsx"
 import {useState} from "react";
@@ -77,7 +77,7 @@ const PaymentFormPage: React.FC<CustomerProps & { orderForm: ReturnType<typeof u
             </ol>
             <div className="right-side1">
                 <h2> Basket </h2>
-                <Total1 items={items} onUpdateTotal={handleUpdateTotal}/>
+                <Total items={items} onUpdateTotal={handleUpdateTotal}/>
                 <h2> Payment</h2>
                 <PaymentForm totalAmount={totalAmount} companyVAT={CompanyVAT}
                              onSavePaymentMethod={handleSavePaymentMethod} orderForm={orderForm}/>
